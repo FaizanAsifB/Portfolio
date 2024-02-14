7bfe9bc09f09e688f5e2d8b2ad0c7705dc7f4db1
FaizanAsifB <faizanasif.dev@gmail.com>
2024-02-14 13:20:02 +0100

    moved moon and sun icon inside switch

--- src/components/ui/ThemeSwitch.tsx
+++ src/components/ui/ThemeSwitch.tsx
@@ -1,38 +1,47 @@
-import { Moon, Sun } from 'lucide-react'
+import { Moon, Sun } from "lucide-react";
 
-import { Switch } from '@/components/ui/switch'
-import { useEffect, useState } from 'react'
+import { Switch } from "@/components/ui/switch";
+import { useEffect, useState } from "react";
 
 function ThemeSwitch() {
-  const [theme, setTheme] = useState<'theme-light' | 'dark' | 'system'>(
-    'theme-light'
-  )
+  const [theme, setTheme] = useState<"theme-light" | "dark" | "system">(
+    "theme-light",
+  );
 
   useEffect(() => {
-    const isDarkMode = document.documentElement.classList.contains('dark')
-    setTheme(isDarkMode ? 'dark' : 'theme-light')
-  }, [])
+    const isDarkMode = document.documentElement.classList.contains("dark");
+    setTheme(isDarkMode ? "dark" : "theme-light");
+  }, []);
 
   useEffect(() => {
     const isDark =
-      theme === 'dark' ||
-      (theme === 'system' &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches)
-    document.documentElement.classList[isDark ? 'add' : 'remove']('dark')
-  }, [theme])
+      theme === "dark" ||
+      (theme === "system" &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches);
+    document.documentElement.classList[isDark ? "add" : "remove"]("dark");
+  }, [theme]);
 
   return (
-    <div className="relative flex items-center gap-1">
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute  h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      <Switch
-        checked={theme === 'dark'}
-        onCheckedChange={() =>
-          setTheme(theme === 'dark' ? 'theme-light' : 'dark')
-        }
-      />
-    </div>
-  )
+    // <div className="relative flex items-center gap-1">
+    <Switch
+      checked={theme === "dark"}
+      onCheckedChange={() =>
+        setTheme(theme === "dark" ? "theme-light" : "dark")
+      }
+    >
+      <div className="relative rounded-full h-4 w-4">
+        <Sun
+          fill="hsl(var(--secondary))"
+          className="absolute inset-0 h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-secondary"
+        />
+        <Moon
+          fill="hsl(var(--secondary))"
+          className="absolute inset-0 h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:-translate-x-5 dark:scale-100 text-secondary"
+        />
+      </div>
+    </Switch>
+    // </div>
+  );
 }
 
-export default ThemeSwitch
+export default ThemeSwitch;
--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -9,7 +9,7 @@ const Switch = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-foreground data-[state=unchecked]:bg-input",
+      "peer  inline-flex gap-0.5 h-6 w-11 shrink-0 cursor-pointer justify-center items-center rounded-full border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 bg-foreground  ",
       className,
     )}
     {...props}
@@ -20,6 +20,7 @@ const Switch = React.forwardRef<
         "pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0",
       )}
     />
+    {props.children}
   </SwitchPrimitives.Root>
 ));
 Switch.displayName = SwitchPrimitives.Root.displayName;
