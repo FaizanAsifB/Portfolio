922015e68d68967db0d973239cd304b375af37d5
FaizanAsifB <faizanasif.dev@gmail.com>
2024-02-21 15:48:21 +0100

    added icon to submit

--- src/components/form/ContactForm.tsx
+++ src/components/form/ContactForm.tsx
@@ -92,7 +92,22 @@ const ContactForm = () => {
           )}
         />
         <div className="w-full grid place-items-center my-8">
-          <Button className="" type="submit">
+          <Button type="submit">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-send"
+            >
+              <path d="m22 2-7 20-4-9-9-4Z" />
+              <path d="M22 2 11 13" />
+            </svg>
             Submit
           </Button>
         </div>
