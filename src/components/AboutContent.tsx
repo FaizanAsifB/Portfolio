568c3b0d793ed88198b2829cd34f4771245f6897
FaizanAsifB <faizanasif.dev@gmail.com>
2024-02-23 19:01:07 +0100

    image adjustment about

--- src/components/AboutContent.tsx
+++ src/components/AboutContent.tsx
@@ -1,19 +1,19 @@
-import { buttonVariants } from "@/components/ui/button.tsx";
-import { useInView } from "framer-motion";
-import { useRef } from "react";
+import { buttonVariants } from '@/components/ui/button.tsx'
+import { useInView } from 'framer-motion'
+import { useRef } from 'react'
 
 const AboutContent = () => {
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
+  const ref = useRef(null)
+  const isInView = useInView(ref, { once: true })
 
   return (
-    <div className="container">
+    <div className='container'>
       <h2
         ref={ref}
         style={{
-          opacity: isInView ? "1" : "0",
-          transform: isInView ? "none" : "translateY(-100px)",
-          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.1s",
+          opacity: isInView ? '1' : '0',
+          transform: isInView ? 'none' : 'translateY(-100px)',
+          transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.1s'
         }}
         // whileInView={{ opacity: 1 }}
         // initial={{ opacity: 0, y: -100 }}
@@ -24,26 +24,26 @@ const AboutContent = () => {
         About Me
       </h2>
       <div
-        className="flex gap-6"
+        className='lg:flex gap-6'
         style={{
-          opacity: isInView ? "1" : "0",
-          transform: isInView ? "none" : "translateX(-400px)",
-          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.1s",
+          opacity: isInView ? '1' : '0',
+          transform: isInView ? 'none' : 'translateX(-400px)',
+          transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.1s'
         }}
       >
-        <div className="w-[400px] h-[400px] ">
+        <div className='lg:w-[400px] lg:h-[400px] w-[80%] mx-auto'>
           <img
-            src="https://plus.unsplash.com/premium_photo-1706896001583-08b5ba33e3be?q=80&w=2676&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt=""
-            className="object-contain w-full h-full"
+            src='https://plus.unsplash.com/premium_photo-1706896001583-08b5ba33e3be?q=80&w=2676&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            alt='Faizan Asif'
+            className='object-contain w-full h-full'
           />
         </div>
         <article
-          className="max-w-[60ch] flex-1 text-xl tracking-wide"
+          className='max-w-[65ch] flex-1 tracking-wider nav:mt-8'
           style={{
-            opacity: isInView ? "1" : "0",
-            transform: isInView ? "none" : "translateX(600px)",
-            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.1s",
+            opacity: isInView ? '1' : '0',
+            transform: isInView ? 'none' : 'translateX(600px)',
+            transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.1s'
           }}
         >
           <p>
@@ -68,38 +68,38 @@ const AboutContent = () => {
             outdoor adventures, or simply sharing laughter at home, every moment
             is precious. I also enjoy playing video game and board games.
           </p>
-          <p className="mb-8">
+          <p className='mb-8'>
             I'm excited to combine my love for learning new technologies with my
             passion for solving problems and continue to grow both personally
             and professionally. Let's collaborate and embark on this journey
             together!
           </p>
           <a
-            href="https://www.dropbox.com/scl/fi/lge99usylkol44drd65nt/Faizan-Asif-Butt-Resume.pdf?rlkey=g6xq1lh182g47cejgb0xrieko&dl=0"
-            target="_blank"
-            className={buttonVariants({ variant: "default" })}
+            href='https://www.dropbox.com/scl/fi/lge99usylkol44drd65nt/Faizan-Asif-Butt-Resume.pdf?rlkey=g6xq1lh182g47cejgb0xrieko&dl=0'
+            target='_blank'
+            className={buttonVariants({ variant: 'default' })}
           >
             <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="lucide lucide-download"
+              xmlns='http://www.w3.org/2000/svg'
+              width='20'
+              height='20'
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='currentColor'
+              strokeWidth='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              className='lucide lucide-download'
             >
-              <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-              <polyline points="7 10 12 15 17 10"></polyline>
-              <line x1="12" x2="12" y1="15" y2="3"></line>
+              <path d='M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4'></path>
+              <polyline points='7 10 12 15 17 10'></polyline>
+              <line x1='12' x2='12' y1='15' y2='3'></line>
             </svg>
             Download CV
           </a>
         </article>
       </div>
     </div>
-  );
-};
-export default AboutContent;
+  )
+}
+export default AboutContent
