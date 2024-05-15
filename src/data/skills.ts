a7f786fd5d8951689e9540dc7ef3f5255f6f52a9
Faizan Asif <faizanasif.dev@gmail.com>
2024-05-15 12:41:00 +0200

    refactor socials and skills

---
+++ src/data/skills.ts
@@ -0,0 +1,18 @@
+export const skills = [
+  // {title:'HTML5',logoName:'html'},
+  { title: 'CSS', logoName: 'css' },
+  { title: 'JavaScript', logoName: 'javascript' },
+  { title: 'React', logoName: 'react' },
+  { title: 'Astro', logoName: 'astro' },
+  { title: 'TypeScript', logoName: 'typescript' },
+  { title: 'Sass', logoName: 'sass' },
+  { title: 'Tailwind', logoName: 'tailwind' },
+  { title: 'Firebase', logoName: 'firebase' },
+  { title: 'PostgreSQL', logoName: 'postgresql' },
+  // {title:'Git',logoName:'git'},
+  { title: 'GitHub', logoName: 'github' },
+  { title: 'MongoDB', logoName: 'mongo' },
+  { title: 'Node.js', logoName: 'node' },
+  { title: 'Express.js', logoName: 'express' },
+  { title: 'Next.js', logoName: 'nextjs' }
+]
---
+++ src/data/socials.ts
@@ -0,0 +1,22 @@
+export const socials = [
+  {
+    title: 'Github',
+    url: 'https://github.com/FaizanAsifB',
+    icon: 'fa-brands fa-github'
+  },
+  {
+    title: 'Linkedin',
+    url: 'https://linkedin.com/in/faizan-butt-41ab051b2',
+    icon: 'fa-brands fa-linkedin'
+  },
+  {
+    title: 'Email',
+    url: 'mailto:FaizanAsif.dev@gmail.com',
+    icon: 'fa-solid fa-at'
+  },
+  {
+    title: 'Discord',
+    url: 'https://discord.com/users/faizan88',
+    icon: 'fa-brands fa-discord'
+  }
+]
