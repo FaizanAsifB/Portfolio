326583de6259d976e86c13499440ca4886206749
Faizan Asif <faizanasif.dev@gmail.com>
2024-05-15 15:21:01 +0200

    about image animation

---
+++ src/components/animations/float-animation.tsx
@@ -0,0 +1,19 @@
+import { CardBody, CardContainer, CardItem } from '@/components/ui/3d-card.tsx'
+
+export function FloatAnimation({ src }: { src: string }) {
+  return (
+    <CardContainer className='inter-var'>
+      <CardBody className='group/card relative h-auto  w-auto rounded-xl border border-black/[0.1] bg-gray-50 p-6 dark:border-white/[0.2] dark:bg-black dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] sm:w-[30rem]  '>
+        <CardItem translateZ='100' className='my-4 w-full'>
+          <img
+            src={src}
+            height='1000'
+            width='1000'
+            className='h-[400px] w-full rounded-xl object-cover group-hover/card:shadow-xl'
+            alt='thumbnail'
+          />
+        </CardItem>
+      </CardBody>
+    </CardContainer>
+  )
+}
