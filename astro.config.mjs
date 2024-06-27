3cb1ab9fe0b9916697eb5f0395aa8a17771a58eb
FaizanAsifB <faizanasif.dev@gmail.com>
2024-06-27 16:50:11 +0200

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
-import icon from 'astro-icon';
-
-import node from "@astrojs/node";
+import node from '@astrojs/node'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import icon from 'astro-icon'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  integrations: [tailwind({
-    applyBaseStyles: true
-  }), react(), icon()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: true
+    }),
+    react(),
+    icon()
+  ],
   adapter: node({
-    mode: "standalone"
+    mode: 'standalone'
   })
-});
+})
