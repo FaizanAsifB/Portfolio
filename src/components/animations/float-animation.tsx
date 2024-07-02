7a10f7fce819db6c7ed493acd2692febabe09151
FaizanAsifB <faizanasif.dev@gmail.com>
2024-07-02 12:46:54 +0200

    about section size

--- src/components/animations/float-animation.tsx
+++ src/components/animations/float-animation.tsx
@@ -1,17 +1,11 @@
 import { CardBody, CardContainer, CardItem } from '@/components/ui/3d-card.tsx'
 
-export function FloatAnimation({ src }: { src: string }) {
+export function FloatAnimation({ children }: { children: any }) {
   return (
     <CardContainer className='inter-var'>
-      <CardBody className='group/card relative h-[360px] w-[335px] rounded-xl border border-black/[0.1] bg-gray-50 dark:border-white/[0.2] dark:bg-black dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1]'>
+      <CardBody className='group/card relative h-[376px] w-[350px] rounded-xl border border-black/[0.1] bg-gray-50 dark:border-white/[0.2] dark:bg-black dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1]'>
         <CardItem translateZ='100' className='w-full'>
-          <img
-            src={src}
-            height='360'
-            width='335'
-            className='rounded-xl object-contain group-hover/card:shadow-xl'
-            alt='imgPath'
-          />
+          {children}
         </CardItem>
       </CardBody>
     </CardContainer>
