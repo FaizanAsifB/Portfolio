f75cae07225d848d2db139c87c23b68fa0744241
FaizanAsifB <faizanasif.dev@gmail.com>
2024-07-02 11:15:34 +0200

    about me image resize

--- src/components/animations/float-animation.tsx
+++ src/components/animations/float-animation.tsx
@@ -3,13 +3,13 @@ import { CardBody, CardContainer, CardItem } from '@/components/ui/3d-card.tsx'
 export function FloatAnimation({ src }: { src: string }) {
   return (
     <CardContainer className='inter-var'>
-      <CardBody className='group/card relative h-auto  w-auto rounded-xl border border-black/[0.1] bg-gray-50 dark:border-white/[0.2] dark:bg-black dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] sm:w-[30rem]  '>
-        <CardItem translateZ='100' className=' w-full'>
+      <CardBody className='group/card relative h-[360px] w-[335px] rounded-xl border border-black/[0.1] bg-gray-50 dark:border-white/[0.2] dark:bg-black dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1]'>
+        <CardItem translateZ='100' className='w-full'>
           <img
             src={src}
-            height='1000'
-            width='1000'
-            className='h-[400px] w-full rounded-xl object-cover group-hover/card:shadow-xl'
+            height='360'
+            width='335'
+            className='rounded-xl object-contain group-hover/card:shadow-xl'
             alt='imgPath'
           />
         </CardItem>
