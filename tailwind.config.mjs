c775032c6ca0621609587427df5f6ee3c4e7a57b
FaizanAsifB <faizanasif.dev@gmail.com>
2024-07-06 19:17:56 +0200

    add lgx breakpoint

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -19,6 +19,14 @@ export default {
         '2xl': '1400px'
       }
     },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      lgx: '1160px',
+      xl: '1280px',
+      '2xl': '1400px'
+    },
     extend: {
       colors: {
         border: 'hsl(var(--border))',
@@ -135,31 +143,6 @@ export default {
     require('tailwind-scrollbar')({
       nocompatible: true,
       preferredStrategy: 'pseudoelements'
-    }),
-    function ({ matchUtilities, theme }) {
-      matchUtilities(
-        {
-          'bg-grid': (value) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`
-          }),
-          'bg-grid-small': (value) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`
-          }),
-          'bg-dot': (value) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
-            )}")`
-          })
-        },
-        {
-          values: flattenColorPalette(theme('backgroundColor')),
-          type: 'color'
-        }
-      )
-    }
+    })
   ]
 }
