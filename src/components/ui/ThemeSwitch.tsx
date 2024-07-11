054daf3a203d21113d979eca4017c39dc6a44b58
FaizanAsifB <faizanasif.dev@gmail.com>
2024-07-11 17:45:41 +0200

    improve accessibility

--- src/components/ui/ThemeSwitch.tsx
+++ src/components/ui/ThemeSwitch.tsx
@@ -25,15 +25,16 @@ function ThemeSwitch() {
       className='relative z-[9999]'
       checked={theme === 'dark'}
       onCheckedChange={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      aria-label={`Switch to ${theme === 'dark' ? 'light' : 'dark'} mode`}
     >
       <div className='relative h-4 w-4 rounded-full'>
         <Sun
           fill='hsl(var(--secondary))'
-          className='absolute inset-0 h-4 w-4  scale-0 text-secondary transition-all dark:-translate-x-5  dark:scale-100'
+          className='absolute inset-0 h-4 w-4 scale-0 text-secondary transition-all dark:-translate-x-5 dark:scale-100'
         />
         <Moon
           fill='hsl(var(--secondary))'
-          className='absolute inset-0 h-4 w-4 rotate-0 scale-100 text-secondary transition-all  dark:scale-0'
+          className='absolute inset-0 h-4 w-4 rotate-0 scale-100 text-secondary transition-all dark:scale-0'
         />
       </div>
     </Switch>
